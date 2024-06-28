d657a6c28209d58543571c88ff7dbce17bd9c73c
mecattaf <thomasmecattaf@gmail.com>
2024-06-27 22:37:42 -0400

    Railway attempt 3

--- clone-content.sh
+++ clone-content.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-# Clone markdown content from private GitHub repository into '/pages'
-git clone https://${ACCESS_TOKEN}@${REPO_URL} tpages
+# Check if the 'pages' directory already exists
+if [ ! -d "pages" ]; then
+  # Clone markdown content from private GitHub repository into '/pages'
+  git clone https://${ACCESS_TOKEN}@${REPO_URL} pages
+else
+  echo "Directory 'pages' already exists. Skipping clone."
+fi
