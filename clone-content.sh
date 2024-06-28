2031d03f3450f06b56cc8cdf756596ba21cbab55
mecattaf <thomasmecattaf@gmail.com>
2024-06-27 22:17:15 -0400

    Railway attempt 2

--- clone-content.sh
+++ clone-content.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Clone markdown content from private GitHub repository into 'examples/my-site/pages'
-git clone https://${ACCESS_TOKEN}@${REPO_URL} examples/my-site/src/pages
+# Clone markdown content from private GitHub repository into '/pages'
+git clone https://${ACCESS_TOKEN}@${REPO_URL} tpages
